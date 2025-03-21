732c9cf196d1444939da6489ce650a416526aa75
segundoTellechea <stellechea@mail.austral.edu.ar>
2025-03-21 12:59:38 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,18 +1,11 @@
-    def math():
+def math():
     a = 57
     b = 7
-    
     c=a+b
     print(c)
-
     print(a-b)
-
     print(a*b)
-
     print((a+b)/2)
-
     print(int(a/b))
-
     print(a%b)
-
     print(a/b)
